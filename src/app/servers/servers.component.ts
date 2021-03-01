8fa74c8c97be286770ddec4fc26cc5f8c3951371
hakokoha <kriskosi123@gmail.com>
2021-03-01 13:24:14 +0200

    ngIf, else

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  username = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +27,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
